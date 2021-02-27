e710ef73171caf8d4495b0e4d4f1abc52199a4d3
Konrad Korus <konradkor@interia.pl>
2021-02-27 15:35:04 +0100

    Initial commit - showin game board

---
+++ main_game.py
@@ -0,0 +1,16 @@
+board_size_x = 7
+board_size_y = 6
+in_a_row = 4
+
+game = [[0 for i in range(board_size_x)] for i in range(board_size_y)]
+
+
+#column_choice = input("Pick a column: ")
+#row_choice = input("Pick a row: ")
+
+#showing game board
+
+print("   "+"  ".join([str(i) for i in range(board_size_x)]))
+
+for i, row in zip(range(board_size_y), game):
+    print(i, row)
